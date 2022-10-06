99ab321ef494d490bced3b51de500b600eb1aab8
STIFF404 <free92001@gmail.com>
2022-10-06 16:51:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ global.limitawal = {
     premium: "Infinity",
     free: 25
 }
-global.thumb = fs.readFileSync('./lib/1.jpg')
+global.thumb = { url: 'https://telegra.ph/file/b908e9916ab3c82cbb09b.png' }
 global.vaze = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
 
 let file = require.resolve(__filename)
